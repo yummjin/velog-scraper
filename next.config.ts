1a6b50e7054f25a3c8afbff4b337d32226777ab4
yummjin <cho4u4o@gmail.com>
2026-01-10 11:34:27 +0900

    feat : add Webpack and Turbopack alias configuration for src directory

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./src"),
+    };
+    return config;
+  },
+  turbopack: {
+    resolveAlias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 };
 
 export default nextConfig;
